6542fd11bd67d8939f08a76f3dc94c7661cb6edc
Rusoid <rusoid@gmail.com>
2022-12-28 16:13:34 +0300

    Камень, ножницы, бумага

--- 1Programm.py
+++ 1Programm.py
@@ -1,1 +1,4 @@
-print("Привет Python")
+dog_name = input("Как зовут вашу собаку? ")
+dog_age = input("Сколько лет вашей собаке? ")
+human_age = int(dog_age) * 7
+print('Вашей собаке', dog_name, 'сейчас', human_age, 'по меркам людей')
