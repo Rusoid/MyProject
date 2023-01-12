a8c268aaeb69d1b0f8cd18ebfaf52d87f5698558
Rusoid <rusoid@gmail.com>
2023-01-12 17:15:44 +0300

    del 1Programm.py

--- 1Programm.py
+++
@@ -1,4 +0,0 @@
-dog_name = input("Как зовут вашу собаку? ")
-dog_age = input("Сколько лет вашей собаке? ")
-human_age = int(dog_age) * 7
-print('Вашей собаке', dog_name, 'сейчас', human_age, 'по меркам людей')
