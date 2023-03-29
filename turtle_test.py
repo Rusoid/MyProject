6a581ca3c87531d0a98c0b01c67d63e0ea649c82
Rusoid <rusoid@gmail.com>
2023-03-29 17:33:53 +0300

    Черепашьи бега 1

--- turtle_test.py
+++ turtle_test.py
@@ -2,6 +2,7 @@ import turtle
 
 slowpoke = turtle.Turtle()
 slowpoke.shape('turtle')
+slowpoke.color('blue')
 pokey = turtle.Turtle()
 pokey.shape('turtle')
 pokey.color('red')
@@ -11,8 +12,13 @@ def make_square(the_turtle):
         the_turtle.forward(100)
         the_turtle.right(90)
 
-make_square(slowpoke)
+def make_spiral(the_turtle):
+    for i in range(0, 36):
+        make_square(the_turtle)
+        the_turtle.right(10)
+
+make_spiral(slowpoke)
 pokey.right(45)
-make_square(pokey)
+make_spiral(pokey)
 
 turtle.mainloop()
